6f72908089f7ce0c034e3c8002077dda2704a8d9
Chris Stinson <cistinson@lcmail.lcsc.edu>
2020-03-08 18:38:29 -0700

    add initial variables and first for loop to create array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
 
+  $("#userSubmissionForm").submit(function(){
+
+    //Prevent default submission of form
+    event.preventDefault();
+
+    //Obtain value from user input and convert to number
+    var resultsValue = parseInt($("#userSubmissionForm input").val());
+
+    //Set value of placeholder paragraph tag
+    var resultsPlaceholder = $("#resultsPlaceholder");
+
+    //Create placeholder variable for results array
+    var resultsArray = [];
+
+    //Loop through all numbers between zero and submitted number
+    for (var i = 0; i <= resultsValue; i++){
+      resultsArray.push(i);
+      console.log("hey");
+    }
+
+    //Input results array to placeholder paragraph
+    resultsPlaceholder.text(resultsArray);
+
+    console.log(resultsArray);
+    console.log("Console.log value:" + resultsValue);
+    console.log("Results Placeholder value:" + resultsPlaceholder.text());
+  });
+
 
 });
